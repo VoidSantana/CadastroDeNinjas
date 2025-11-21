5bab2117690c73535691b1e38b16f277b3121825
VoidSantana <jpnsantanna2@gmail.com>
2025-11-20 21:09:19 -0300

    feat: Cria uma Interface (MissoesRepository) e uma Interface (NinjaRepository)

---
+++ src/main/java/dev/santana/CadastroDeNinjas/Missoes/Controller/MissoesRepository.java
@@ -0,0 +1,6 @@
+package dev.santana.CadastroDeNinjas.Missoes.Controller;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+}
