bb2222768af4de69f963ff377680ef580d32fb14
VoidSantana <jpnsantanna2@gmail.com>
2025-12-04 23:24:05 -0300

    feat: Adiciona Migrations tb_cadastro incluindo a coluna de rank

--- src/main/java/dev/santana/CadastroDeNinjas/Ninjas/Controller/NinjaModel.java
+++ src/main/java/dev/santana/CadastroDeNinjas/Ninjas/Controller/NinjaModel.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 
 //Entity transforma uma classe em uma entidade do banco de dados
 // JPA = Java Persistence API
